9ed22a7a396b6e677693781d8a99ecb0cc4c1db6
daxapps <daxapps777@gmail.com>
2016-12-28 05:39:51 -0600

    Nonnull & Nullable

--- Book/Book.h
+++ Book/Book.h
@@ -11,12 +11,13 @@
 
 @interface Book : NSObject
 
-@property (nonatomic) NSString *title;
-@property (nonatomic) Person *author;
+@property (nonatomic, copy) NSString * _Nonnull title;
+@property (nonatomic) Person * _Nonnull author;
+@property (nonatomic) Person * _Nullable editor;
 @property (nonatomic) int yearOfPublication;
 
--(instancetype)initWithTitle:(NSString*)title
-                      author:(Person*)author
+-(instancetype _Nonnull)initWithTitle:(NSString* _Nonnull)title
+                      author:(Person* _Nonnull)author
                         year:(int)year;
 
 @end
